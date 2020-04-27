b7fcfcaa5820473d5aa78b71dda8730fef134b3d
pweskley25 <pauloweskley.dev@gmail.com>
2020-04-27 12:33:45 -0300

    Primeiro commit.

---
+++ src/test/java/br/com/lendarioneo/curso_java_spring/CursoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lendarioneo.curso_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
