fdf9c50d81279e2278d53b2fe12391b8f7821521
pweskley25 <pauloweskley.dev@gmail.com>
2020-04-29 15:55:57 -0300

    Inclusão do H2

--- src/main/java/br/com/lendarioneo/curso_java_spring/entities/User.java
+++ src/main/java/br/com/lendarioneo/curso_java_spring/entities/User.java
@@ -1,10 +1,22 @@
 package br.com.lendarioneo.curso_java_spring.entities;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
